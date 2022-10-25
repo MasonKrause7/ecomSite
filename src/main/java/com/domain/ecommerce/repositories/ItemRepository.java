2fb3f66651cb2f2c69fd842f0b71380c0c6301b9
Candelario <candelarioa42@gmail.com>
2022-10-25 18:19:46 -0500

    "added packages:models,service, and repository"

---
+++ src/main/java/com/domain/ecommerce/repositories/ItemRepository.java
@@ -0,0 +1,9 @@
+package com.domain.ecommerce.repositories;
+
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends JpaRepository<AbstractReadWriteAccess.Item,Long> {
+}
