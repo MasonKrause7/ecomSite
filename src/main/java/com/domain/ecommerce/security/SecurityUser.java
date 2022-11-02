dfea45421b73c32d32ff7015d819a6b26a1d1057
Candelario <candelarioa42@gmail.com>
2022-11-02 12:17:10 -0500

    "updated User fields"

--- src/main/java/com/domain/ecommerce/security/SecurityUser.java
+++ src/main/java/com/domain/ecommerce/security/SecurityUser.java
@@ -31,7 +31,7 @@ public class SecurityUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getUserName();
+        return user.getEmail();
     }
 
     @Override
